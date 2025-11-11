1575a922c1ddbf5685bf2125abb4985aaf33b17a
Angelo Garcia <garciaangelo110996@gmail.com>
2025-11-11 13:40:46 +0800

    explore layouts for gallery

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,27 +1,27 @@
 export default function AboutSection() {
     return (
-        <section id="about" className="h-screen items-center justify-center">
-
-
-           
-
-             <div className="grid grid-cols-3 grid-rows-3 gap-2 h-full w-full rounded-2xl">
-    {/* Div 1 */}
-    <div className="col-span-2 row-span-2 bg-[url('/stoneOven.jpg')] bg-cover bg-center rounded-2xl"></div>
-
-    {/* Div 2 */}
-    <div className="bg-[url('/handMade.jpg')] bg-cover bg-center rounded-2xl"></div>
-
-    {/* Div 3 */}
-    <div className="bg-[url('/BreadHome.jpg')] bg-cover bg-center rounded-2xl"></div>
-
-    {/* Div 4 */}
-    <div className="col-span-2 bg-[url('/zoomBread.jpg')] bg-cover bg-center rounded-2xl"></div>
-
-    {/* Div 5 */}
-    <div className="bg-[url('/handMade.jpg')] bg-cover bg-center rounded-2xl"></div>
+       <section  id="about" className="h-screen flex items-center justify-between bg-orange-400 p-10">
+  {/* Left content area */}
+  <div className="bg-white rounded-r-[100px] flex flex-col justify-center px-16 py-10 w-1/2 h-[80%] shadow-lg">
+    <h1 className="text-4xl font-bold text-gray-800 leading-snug">
+      Have no time to prepare <span className="text-orange-500">food?</span>
+    </h1>
+    <p className="text-gray-600 mt-4">
+      Choose one of our plans, enter delivery time, and enjoy delicious food
+      without leaving your home!
+    </p>
+    <div className="mt-8 flex gap-4">
+      <button className="bg-orange-500 text-white px-6 py-3 rounded-full hover:bg-orange-600">
+        Order food
+      </button>
+      <button className="text-gray-600 hover:underline">Read more</button>
+    </div>
   </div>
-        </section>
+
+  {/* Right image area */}
+  <div className="w-1/2 h-[80%] bg-[url('/sliceOfCake.jpg')] bg-cover bg-center rounded-l-[100px] shadow-lg"></div>
+</section>
+
 
     );
 }
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,7 +1,7 @@
 
 export default function MenuSection (){
     return (
-        <section id="menu" className="h-screen flex items-center justify-center bg-gray-600">
+        <section id="menu" className="h-screen flex items-center justify-center bg-linear-to-b from-orange-80 to-orange-200">
             <h1>This is Menu Page</h1>
         </section>
     );
