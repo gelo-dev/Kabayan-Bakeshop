6a649229d58c4c42641d2faad225e38c587c9ce1
Angelo Garcia <garciaangelo110996@gmail.com>
2025-11-12 13:21:20 +0800

    Start Menu Layout

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -7,30 +7,31 @@ export default function GallerySection(){
 
 
     return(
-        <section id="gallery" className="h-screen flex  bg-linear-to-b from-orange-80 to-orange-200">
+        <section id="gallery" className="h-screen flex  bg-[url('/cookies.jpg')] bg-cover bg-center">
+            
                 <div className="flex flex-wrap gap-2  w-full h-full mt-2 justify-center  ">
                     <div className=" border-2 border-white shadow-xl  bg-[url('/cookies.jpg')] bg-cover bg-center brightness-90 w-[25%] h-[45%] bg-gray-300 flex items-center justify-center text-gray-700 font-semibold hover:scale-103 hover:brightness-110">
-                    The building
+                    
                     </div>
 
                     <div className=" border-2 border-white shadow-xl  bg-[url('/interiorStore.jpg')] bg-cover bg-center hover:scale-101 hover:brightness-110  w-[45%] h-[45%] bg-gray-300 flex items-center justify-center text-gray-700 font-semibold ">
-                    Rhythm of House
+                    
                     </div>
 
                     <div className=" border-2 border-white shadow-xl  bg-[url('/enseymada.jpg')] bg-cover bg-center hover:scale-101 hover:brightness-110 w-[25%] h-[45%] bg-gray-300 flex items-center justify-center text-gray-700 font-semibold ">
-                    Photovoltaic farm
+                    
                     </div>
 
                     <div className=" border-2 border-white shadow-xl  bg-[url('/sliceOfCake.jpg')] bg-cover bg-center hover:scale-101 hover:brightness-110 w-[35%] h-[45%] bg-gray-300 flex items-center justify-center text-gray-700 font-semibold ">
-                    Agricultural workshop
+                    
                     </div>
 
                     <div className=" border-2 border-white shadow-xl  bg-[url('/breadSharing.jpg')] bg-cover bg-center hover:scale-101 hover:brightness-110 w-[25%] h-[45%] bg-gray-300 flex items-center justify-center text-gray-700 font-semibold ">
-                    Eco residence
+                    
                     </div>
 
                     <div className=" border-2 border-white shadow-xl  bg-[url('/BreadHome.jpg')] bg-cover bg-center hover:scale-101 hover:brightness-110 w-[35%] h-[45%] bg-gray-300 flex items-center justify-center text-gray-700 font-semibold">
-                    A residence in a village
+                    
                     </div>
                 </div>
         </section>
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,9 +1,46 @@
 
 export default function MenuSection (){
     return (
-        <section id="menu" className="h-screen flex items-center justify-center bg-linear-to-b from-orange-80 to-orange-200">
-            <h1>This is Menu Page</h1>
-        </section>
+        <section
+            id="menu"
+            className="h-screen flex flex-col gap-8 items-center justify-center relative bg-amber-600"
+            >
+                <div
+                    className="absolute inset-0 bg-[url('/pizzaDough.jpg')] bg-cover bg-center"
+                    style={{ opacity: 0.4 }} // Adjust this value
+                ></div>
+
+
+            <div className="flex gap-8 w-3/4 justify-center relative z-10 " >
+{/* Menu for cakes */}
+                <div className="flex-1 h-140 border-2 border-white bg-transparent rounded-4xl hover:shadow-2xl">                                        
+                        <div className="bg-transparent h-1/2 items-end justify-center ">
+                            <img src="/chocolateCake.png"
+                                alt="Chocolate Cake"
+                                className="w-[65%] object-contain translate-y-8 translate-x-12" />
+                        </div>
+
+                        <div className="bg-white h-1/2 rounded-b-4xl items-center justify-center text-center flex flex-col gap-3">
+                            <h1 className="font-serif font-extralight text-2xl ">CAKES</h1>
+                            <h4 className="p-7 font-sans">"Indulge in our soft, moist cakes baked to perfection and layered with rich frosting. Each slice melts in your mouth — perfect for every celebration or sweet craving"</h4>
+                        </div>                                        
+                </div>
+
+
+                <div className="flex-1 h-140 bg-amber-400 rounded-4xl shadow-md"></div>
+                <div className="flex-1 h-140 bg-amber-600 rounded-4xl shadow-md"></div>
+              
+               
+            </div>
+            <div>
+                <button className="h-12 w-64 bg-white text-gray-900 font-semibold rounded-full shadow-md hover:bg-gray-100 hover:shadow-lg transition duration-300 ease-in-out">
+                    View Menu
+                    </button>
+
+            </div>
+             
+            </section>
+
     );
 }
 
