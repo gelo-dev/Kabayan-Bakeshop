b716dffbd3456081b501c03a29d5f690efba1b39
Angelo Garcia <garciaangelo110996@gmail.com>
2025-12-27 14:02:50 +0800

    Contact Page Progress 1

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,47 @@
 
 import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+import { MdLocationOn ,MdPhone, MdEmail } from "react-icons/md";
 import ContactFormSection from "../components/ContactPageComponent/contactForm";
 
 export default function ContactSection() {
   return (
     <section id="contact" className="h-screen grid grid-cols-2 p-2">
         <div
-         
         className="bg-transparent flex flex-col p-4 items-center justify-center">
           <div className=" flex flex-col text-white 
                             items-center justify-center h-1/2" >
+                              
               <span className="text-[5rem] font-serif">CONTACT US</span>
               <span className="text-xl font-light">We'd love to hear from you! </span>
           </div>
           <div className="flex gap-5 h-1/4 w-full p-4">
-              <div className="bg-white w-full">Location Of Branches</div>
-              <div className="bg-white w-full">Email and Phone</div>
+              <div className=" w-full rounded-2xl flex flex-col items-center justify-center ">
+                <MdLocationOn 
+                size={30} 
+                className="text-white"/>
+                <span className="text-white text-lg font-serif"> Our Branches</span>
+                <span className="text-white text-center text-sm font-light">Find our bakery locations and visit us near you.</span>
+                <button className=" outline outline-white text-white 
+                                    text-xs w-35 h-8 rounded-xl mt-5 
+                                    flex items-center justify-center
+                                    hover:bg-amber-600 hover:text-sm">
+                            View locations →
+                </button>
+                  
+              </div>
+              <div className=" w-full flex flex-col items-center justify-center">
+                  <div className="flex gap-2">
+                    <MdPhone size={20}
+                    className="text-white"/>
+                    <p className="text-white">+63 912-345-6789</p>                        
+                  </div>
+                  <div className="flex gap-2">
+                    <MdEmail size={20}
+                    className="text-white"/>
+                    <p className="text-white">contact@kabayanbakeshop.com</p>                        
+                  </div>
+                  <p className="text-white text-center font-light text-sm">Email us or give us a call for inquiries.</p>
+              </div>
               <div className="bg-white w-full">FAQ</div>
           </div>
         </div>
